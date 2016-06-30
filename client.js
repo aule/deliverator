047b4d41c5c660008559a2ca763395900597c990
Joseph Taylor <josephta@uk.ibm.com>
2016-06-30 15:34:19 +0100

    Updated client to be more structured and join topics

--- client.js
+++ client.js
@@ -1,32 +1,45 @@
 var IOClient = require('socket.io-client');
 var exec = require('child_process').exec;
 var fs = require('fs');
-var buffer_on = new Buffer([ 255, 1, 1]);
-var buffer_off = new Buffer([ 255, 1, 0]);
 var connection = IOClient("ws://jessies.personal.jcarter.uk0.bigv.io:4567", {'transports': ['websocket']});
 
-connection.on('connect', function(){
-    console.log("connected");
-    //console.log(connection);
-});
-
-connection.on('disconnect', function(){
-    console.log("lost connection");
-});
-
-connection.on('updates', function(data){
-    console.log("MESSAGE RECEIVED");
-    console.log(data);
-
+function trigger(){
+    var buffer_on = new Buffer([ 255, 1, 1]);
+    var buffer_off = new Buffer([ 255, 1, 0]);
     try {
-      var wstream = fs.createWriteStream('/dev/ttyUSB0');
-      wstream.write(buffer_on);
-      setTimeout(function() {
-        wstream.write(buffer_off);
-        wstream.end;
-      }, 200);
+        var wstream = fs.createWriteStream('/dev/ttyUSB0');
+        wstream.write(buffer_on);
+        setTimeout(function() {
+            wstream.write(buffer_off);
+            wstream.end();
+        }, 200);
     }
     catch (e) {
-      console.log(e);
+        console.log("Error with trigger.");
+        console.log(e);
     }
-});
+}
+
+function connect(url, topics){
+    var opts = {
+        'transports': ['websocket'],
+        'query': {'topics': topics}
+    };
+    var connection = IOClient(url, opts);
+
+    connection.on('connect', function(){
+        console.log("Connected to " + url);
+    });
+
+    connection.on('disconnect', function(){
+        console.log("Lost connection to " + url);
+    });
+
+    connection.on("updates", function(data){
+        console.log("MESSAGE RECEIVED");
+        console.log(data);
+        trigger()
+    });
+}
+
+connect("ws://jessies.personal.jcarter.uk0.bigv.io:4567", ["bob", "sally"]);
