092b52798ee6cb5fcf79ca4922d4098ec9894aac
Joseph Taylor <josephta@uk.ibm.com>
2016-06-30 16:04:29 +0100

    Added params over cmdline and connection error message

--- client.js
+++ client.js
@@ -30,20 +30,44 @@ function connect(url, topics){
         'query': {'topics': topics}
     };
     var connection = IOClient(url, opts);
+    var url_tested = false;
 
     connection.on('connect', function(){
         console.log("Connected to " + url);
+        url_tested = true;
     });
 
     connection.on('disconnect', function(){
         console.log("Lost connection to " + url);
     });
 
-    connection.on("updates", function(data){
+    connection.on('updates', function(data){
         console.log("MESSAGE RECEIVED");
         console.log(data);
-        trigger()
+        trigger();
+    });
+
+    connection.on('connect_error', function(error){
+        console.log("Connection error.");
+        console.log(error);
+        if(!url_tested){
+            console.log("URL might not be valid. Please try again.");
+            process.exit(2);
+        }
     });
 }
 
-connect("ws://jessies.personal.jcarter.uk0.bigv.io:4567", ["bob", "sally"]);
+function start(){
+    var args = process.argv.slice();
+    var node = args.shift();
+    var script = args.shift();
+    var url = args.shift();
+    var topics = args;
+    if(!url){
+        console.log("Usage: " + node + " " + script + " <url> <topic> [... <topic>]");
+        process.exit(1);
+    }
+    connect(url, topics);
+}
+
+start();
