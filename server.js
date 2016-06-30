6a3fcb32b9d220663bd6c1f252485135a6b97f8e
Joseph Taylor <josephta@uk.ibm.com>
2016-06-30 15:27:39 +0100

    Added topic based server

--- server.js
+++ server.js
@@ -1,22 +1,47 @@
 var http = require('http');
-var IOServer = require('socket.io');
+var io = require('socket.io');
+var express = require('express');
+var body_parser = require('body-parser');
 
-var server = http.createServer();
-var io_server = new IOServer(server);
+var app = express();
+var server = http.Server(app);
+var io_server = io(server);
 
-server.on('request', function (request, response){
-    console.log(request.url)
-    if(request.url == '/send'){
-        io_server.emit("updates", {"message": "Moo"});
-        response.end("Sending");
-
-    }
-});
 
 io_server.on('connect', function (socket){
-    console.log("Connection started");
-    socket.emit("updates", {"message": "OI!"});
+    var id = socket.id;
+    console.log("Connection started: " + id);
+    socket.on('disconnect', function (socket){
+        console.log("Connection terminated: " + id);
+    });
 });
 
-server.listen(4567);
-console.log("Waiting on port 4567");
+
+io_server.use(function (socket, next){
+    topics = socket.handshake.query.topics;
+    if(topics){
+        topics.split(",").forEach(function (topic){
+            console.log("Joining " + socket.id + " to " + topic);
+            socket.join(topic);
+        });
+    }
+    next();
+});
+
+app.use(body_parser.urlencoded({ extended: false }))
+app.use(body_parser.json())
+
+app.get('/send/:topic', function (request, response){
+    response.setHeader("content-type", "text/html");
+    response.end('<form method="POST"><textarea name="testdata"></textarea><input type="submit"></form>');
+});
+
+app.post('/send/:topic', function (request, response){
+    var topic = request.params['topic'];
+    console.log("Sending to " + topic);
+    io_server.to(topic).emit("updates", request.body);
+    response.end("Sent");
+});
+
+server.listen(3000);
+console.log("Waiting on port 3000");
